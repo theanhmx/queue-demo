4f8eadcf117efcaa0b3259bc46b794186cfa4bcc
Diep The Anh <anh.diep@wizeline.com>
2019-03-01 20:54:19 +0700

    priority queue

--- index.js
+++ index.js
@@ -16,19 +16,44 @@ const app = new Vue({
   },
   methods: {
     enqueue: function (item) {
-      this.queue.push(item);
+      let contain = false;
+
+      // iterating through the entire
+      // item array to add element at the
+      // correct location of the Queue
+      for (var i = 0; i < this.queue.length; i++) {
+        if (this.queue[i].priority > item.priority) {
+          // Once the correct location is found it is
+          // enqueued
+          this.queue.splice(i, 0, item);
+          contain = true;
+          break;
+        }
+      }
+
+      // if the element have the highest priority
+      // it is added at the end of the queue
+      if (!contain) {
+        this.queue.push(item);
+      }
     },
     dequeue: function () {
-      return this.queue.shift();
-  
+      // return the dequeued element 
+      // and remove it. 
+      // if the queue is empty 
+      // returns Underflow 
+      if (this.queue.length === 0) 
+        return 'Underflow'; 
+      return this.queue.shift(); 
     },
     peek: function () {
       return (this.queue[0] ? this.queue[0] : undefined);
     },
-    writeToFile: function(color, text) {
+    writeToFile: function (color, text, priority) {
       this.enqueue({
         color,
-        text
+        text,
+        priority
       });
       switch (color) {
         case 'red':
